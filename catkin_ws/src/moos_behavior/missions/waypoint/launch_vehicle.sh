78f3733ab539bf0c3c2564ef82d405ebb68b3069
Tony Hsiao <tonycar12002@gmail.com>
2019-01-07 17:32:13 +0800

    add send and receive data between two vehicle

--- catkin_ws/src/moos_behavior/missions/waypoint/launch_vehicle.sh
+++ catkin_ws/src/moos_behavior/missions/waypoint/launch_vehicle.sh
@@ -24,12 +24,12 @@ echo "SHORE_PORT = " $SHORE_PORT ", VEH_PORT1 = " $VEH_PORT1 ", VEH_PORT2 = " $V
 #  Part 2: Launch the processes
 #-------------------------------------------------------
 nsplug vehicle.moos duckiepond_$VEH_NAME1.moos -f \
-    VEH_NAME=$VEH_NAME1  VEH_PORT=$VEH_PORT1    \
+    VNAME=$VEH_NAME1  VEH_PORT=$VEH_PORT1    \
     VEH_IP=$VEH_IP1      SHORE_IP=$SHORE_IP \
     SHORE_PORT=$SHORE_PORT START_POS=$START_POS1
 
 nsplug vehicle.moos duckiepond_$VEH_NAME2.moos -f \
-    VEH_NAME=$VEH_NAME2    VEH_PORT=$VEH_PORT2 \
+    VNAME=$VEH_NAME2    VEH_PORT=$VEH_PORT2 \
     VEH_IP=$VEH_IP2      SHORE_IP=$SHORE_IP \
     SHORE_PORT=$SHORE_PORT START_POS=$START_POS2
 
@@ -39,14 +39,14 @@ nsplug vehicle.bhv duckiepond_$VEH_NAME1.bhv -f VNAME=$VEH_NAME1     \
 nsplug vehicle.bhv duckiepond_$VEH_NAME2.bhv -f VNAME=$VEH_NAME2     \
     START_POS=$START_POS2   
 
-printf "Launching the %s MOOS Community (WARP=%s) \n"  duckiepond_${VEH_NAME1} $TIME_WARP
-pAntler duckiepond_${VEH_NAME1}.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+printf "Launching the %s MOOS Community (WARP=%s) \n"  duckiepond_$VEH_NAME1 $TIME_WARP
+pAntler duckiepond_$VEH_NAME1.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
 
-printf "Launching the %s MOOS Community (WARP=%s) \n"  duckiepond_${VEH_NAME2} $TIME_WARP
-pAntler duckiepond_${VEH_NAME2}.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+printf "Launching the %s MOOS Community (WARP=%s) \n"  duckiepond_$VEH_NAME2 $TIME_WARP
+pAntler duckiepond_$VEH_NAME2.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
 
 
-uMAC duckiepond_${VEH_NAME1}.moos
+uMAC duckiepond_$VEH_NAME1.moos
 
 
 printf "Killing all processes ... \n"
