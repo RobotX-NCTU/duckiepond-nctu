1f6700de32d38ddcea79baae2c3e9c16ee168c52
huangjuite <huangjuite@gmail.com>
2019-01-18 11:28:12 +0000

    change message name

--- catkin_ws/src/joystick_control/src/heading_to_motor_cmd.py
+++ catkin_ws/src/joystick_control/src/heading_to_motor_cmd.py
@@ -2,18 +2,18 @@
 import rospy
 import math
 
-from duckiepond.msg import Motor_cmd,Heading
+from duckiepond.msg import MotorCmd,Heading
 
-class MotorCmd(object):
+class Commander(object):
     def __init__(self):
         self.node_name = rospy.get_name()
         rospy.loginfo("[%s] Initializing " %(self.node_name))
         
-        self.pub_motor_cmd = rospy.Publisher("motor_cmd",Motor_cmd,queue_size=1)
+        self.pub_motor_cmd = rospy.Publisher("motor_cmd",MotorCmd,queue_size=1)
         self.sub_heading = rospy.Subscriber("boat_heading",Heading,self.cbHeading,queue_size=1)
 
     def cbHeading(self,msg):
-        mcd_msg = Motor_cmd()
+        mcd_msg = MotorCmd()
         speed = msg.speed*math.sin(msg.phi)
         difference = msg.speed*math.cos(msg.phi)
         mcd_msg.left = max(min(speed - difference , 1),-1)
@@ -23,5 +23,5 @@ class MotorCmd(object):
 
 if __name__ == "__main__":
     rospy.init_node("heading_to_motor_cmd")
-    commander = MotorCmd()
+    commander = Commander()
     rospy.spin()
