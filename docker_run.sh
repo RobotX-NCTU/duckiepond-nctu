edc7be6f1b262253118820c7595856be8fe48cde
huangjuite <huangjuite@gmail.com>
2019-01-25 00:20:09 +0800

    update shell script

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 if [ $# -gt 0 ]; then
-	# provided a hostname, use it as ROS_MASTER_URI
 	docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/hostname  juite/duckiepond:$1 
 else
 	echo "please provide docker tag name."
