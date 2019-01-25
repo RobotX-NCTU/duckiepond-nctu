4688f47137018307358ceabbb0e0fff5f37e2235
huangjuite <huangjuite@gmail.com>
2019-01-25 17:29:59 +0800

    modify motor control

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ $# -gt 0 ]; then
-	docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/hostname  juite/duckiepond:$1 
+	docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/root/duckiepond-nctu  juite/duckiepond:$1 
 else
 	echo "please provide docker tag name."
 fi
