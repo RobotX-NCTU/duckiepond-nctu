8240701b36cfff5a30a7a80b48620806df4d1c8f
tonycar12002 <tonycar12002@gmail.com>
2019-02-09 21:42:45 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,40 +1,6 @@
 #!/usr/bin/env bash
-for ARGI; do
-    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-        printf "  --brian_pi3, -b_pi         \n" 
-        printf "  --monica_pi3, -m_pi         \n"
-		printf "  --brian_tx2, -b_tx         \n" 
-		printf "  --monica_tx2, -m_tx         \n"  
-		printf "  --laptop, -pc         \n"  
-        printf "  --help, -h         \n" 
-        exit 0;
-    elif [ "${ARGI}" = "-b_pi" ] ; then
-		docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro \
-		 -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/root/duckiepond-nctu  \
-		 juite/duckiepond:pi3
-
-    elif [ "${ARGI}" = "-m_pi" ] ; then 
-		docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro \
-		 -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/root/duckiepond-nctu  \
-		 juite/duckiepond:pi3
-
-    elif [ "${ARGI}" = "-b_tx" ] ; then 
-		docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro \
-		 -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/root/duckiepond-nctu  \
-		 juite/duckiepond:tx2
-
-	elif [ "${ARGI}" = "-m_tx" ] ; then 
-		docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro \
-		 -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/root/duckiepond-nctu  \
-		 juite/duckiepond:tx2
-
-    elif [ "${ARGI}" = "-pc" ] ; then 
-		docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro \
-		 -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/root/duckiepond-nctu  \
-		--add-host eastgod:192.168.2.103 juite/duckiepond:laptop
-
-    else 
-        printf "Bad Argument: %s \n" $ARGI
-        exit 0
-    fi
-done
+if [ $# -gt 0 ]; then
+	docker run --rm -it --net=host --privileged -v /dev:/dev -v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER/duckiepond-nctu:/root/duckiepond-nctu  juite/duckiepond:$1 
+else
+	echo "please provide docker tag name."
+fi
