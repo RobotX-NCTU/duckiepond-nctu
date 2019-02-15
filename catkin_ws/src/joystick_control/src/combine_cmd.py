6e87ecc718a277962e90f248f1fdcffb332d82a7
huangjuite <huangjuite@gmail.com>
2019-02-15 19:00:53 +0800

    fix no msg receive

--- catkin_ws/src/joystick_control/src/combine_cmd.py
+++ catkin_ws/src/joystick_control/src/combine_cmd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from duckiepond.msg import MotorCmd
+from std_msgs.msg import Header
 
 class Combine_moos_ssd():
     def __init__(self):
@@ -12,6 +13,9 @@ class Combine_moos_ssd():
         self.cmd = MotorCmd()
         self.moos_ratio = 0.5
         self.ssd_ratio = 1 - self.moos_ratio
+
+        self.moos_time = rospy.Time.now()
+        self.ssd_time = rospy.Time.now()
         
         self.sub_moos = rospy.Subscriber("moos_drive",MotorCmd,self.cb_moos,queue_size=1)
         self.sub_ssd = rospy.Subscriber("ssd_drive",MotorCmd,self.cb_ssd,queue_size=1)
@@ -20,16 +24,30 @@ class Combine_moos_ssd():
         self.timer = rospy.Timer(rospy.Duration(0.2),self.cb_publish)
 
     def cb_moos(self,msg):
+        self.moos_time = rospy.Time.now()
         self.moos_cmd = msg
 
     def cb_ssd(self,msg):
+        self.ssd_time = rospy.Time.now()
         self.ssd_cmd = msg
     
     def cb_publish(self,event):
-        self.cmd.right = self.moos_cmd.right * self.moos_ratio
-        self.cmd.left = self.moos_cmd.left * self.moos_ratio
-        self.cmd.right += self.ssd_cmd.right * self.ssd_ratio
-        self.cmd.left += self.ssd_cmd.left * self.ssd_ratio
+        time_now = rospy.Time.now()
+        moos_diff = time_now.to_sec() - self.moos_time.to_sec()
+        ssd_diff = time_now.to_sec() - self.ssd_time.to_sec()
+
+        if moos_diff<1.5 and  ssd_diff<1.5:
+            self.cmd.right = self.moos_cmd.right * self.moos_ratio
+            self.cmd.left = self.moos_cmd.left * self.moos_ratio
+            self.cmd.right += self.ssd_cmd.right * self.ssd_ratio
+            self.cmd.left += self.ssd_cmd.left * self.ssd_ratio
+        elif moos_diff>1.5 and ssd_diff<1.5:
+            self.cmd = self.ssd_cmd
+        elif moos_diff<1.5 and ssd_diff>1.5:
+            self.cmd = self.moos_cmd
+        else:
+            self.cmd.right = 0
+            self.cmd.left = 0
         self.pub_cmd.publish(self.cmd)
         
 
