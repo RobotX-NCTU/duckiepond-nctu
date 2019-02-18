25897a7b1e137238afecf0a3ce05f371e585baaf
Tony Hsiao <tonycar12002@gmail.com>
2019-02-18 15:38:32 +0800

    update d_phi visual

---
+++ catkin_ws/src/analysis/src/d_phi_extract.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import numpy as np
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point
+from message_filters import ApproximateTimeSynchronizer, TimeSynchronizer
+import message_filters
+import math
+import tf
+fo = open("combine_cirlce_ccw.txt", "w")
+
+def cb_odom(msg_folow, msg_lead):
+    dis = distance(msg_folow.pose.pose.position, msg_lead.pose.pose.position)
+    phi = angle(msg_folow.pose.pose.orientation, msg_lead.pose.pose.orientation)
+    
+    print(dis, phi)
+
+    text = str(dis) + " " + str(phi) + '\n'
+    fo.write(text)
+
+def angle(orientation_1, orientation_2):
+    q = (orientation_1.x, orientation_1.y, orientation_1.z, orientation_1.w)
+    yaw_1 = tf.transformations.euler_from_quaternion(q)[2]
+
+    q = (orientation_2.x, orientation_2.y, orientation_2.z, orientation_2.w)
+    yaw_2 = tf.transformations.euler_from_quaternion(q)[2]
+
+    yaw_dff = yaw_2 - yaw_1
+
+    if yaw_dff >= math.pi:
+        yaw_dff -= 2*math.pi
+    elif yaw_dff <= -math.pi:
+        yaw_dff += 2*math.pi
+
+    return yaw_dff
+
+def distance(position_1, position_2):
+    x = position_1.x - position_2.x
+    y = position_1.y - position_2.y
+    return math.sqrt(x*x + y*y)
+
+if __name__ == "__main__":
+    rospy.init_node("odometry_plot",anonymous=False)
+
+    point_list_follow = []
+    point_list_lead = []
+
+    # Square
+    route_list = [(0, 7), (0, -21), (28, -21), (28, 7), (0, 7)]
+
+    # Circle
+    sub_odom_follow = message_filters.Subscriber("/MONICA/localization_gps_imu/odometry", Odometry)
+    sub_odom_lead = message_filters.Subscriber("/BRIAN/localization_gps_imu/odometry", Odometry)
+
+    ats = ApproximateTimeSynchronizer((sub_odom_follow, sub_odom_lead), queue_size = 1, slop = 0.5)
+    ats.registerCallback(cb_odom)
+
+    rospy.spin()
+
+fo.close()
--- catkin_ws/src/analysis/src/odometry_visual.py
+++ catkin_ws/src/analysis/src/odometry_visual.py
@@ -5,14 +5,20 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
 
-def cb_odom(msg):
+def cb_odom_follow(msg):
     position = msg.pose.pose.position
-    point_list.append(position)
+    point_list_follow.append(position)
+
+def cb_odom_lead(msg):
+    position = msg.pose.pose.position
+    point_list_lead.append(position)
 
 def draw(event):
     draw_route()
-    if len(point_list) != 0:
-        draw_odom()
+    if len(point_list_follow) != 0:
+        draw_odom_folow()
+    if len(point_list_lead) != 0:
+        draw_odom_lead()    
 
 def draw_route():
     route_marker = Marker()
@@ -53,7 +59,7 @@ def draw_route():
 
     pub_route_line.publish(route_marker)
 
-def draw_odom():
+def draw_odom_folow():
     
     line_marker = Marker()
     line_marker.header.frame_id = "odom"
@@ -83,30 +89,68 @@ def draw_odom():
     line_marker.pose.position.y = 0.0
     line_marker.pose.position.z = 0.0
         
-    for point in point_list:
+    for point in point_list_follow:
         x = point.x
         y = point.y
         pt = Point(x, y, 0)
         line_marker.points.append(pt)
-    pub_odom_line.publish(line_marker)
+    pub_odom_line_follow.publish(line_marker)
 
+def draw_odom_lead():
+    
+    line_marker = Marker()
+    line_marker.header.frame_id = "odom"
+    line_marker.header.stamp = rospy.Time.now()
+    line_marker.action = line_marker.ADD
+    line_marker.type = line_marker.LINE_STRIP
+
+    # maker scale
+    line_marker.scale.x = 0.05
+    line_marker.scale.y = 0.05
+    line_marker.scale.z = 0.05
+
+    # marker color
+    line_marker.color.a = 1.0
+    line_marker.color.r = 0.0
+    line_marker.color.g = 0.0
+    line_marker.color.b = 1.0
+
+    # marker orientaiton
+    line_marker.pose.orientation.x = 0.0
+    line_marker.pose.orientation.y = 0.0
+    line_marker.pose.orientation.z = 0.0
+    line_marker.pose.orientation.w = 1.0
+
+    # marker position
+    line_marker.pose.position.x = 0.0
+    line_marker.pose.position.y = 0.0
+    line_marker.pose.position.z = 0.0
+        
+    for point in point_list_lead:
+        x = point.x
+        y = point.y
+        pt = Point(x, y, 0)
+        line_marker.points.append(pt)
+    pub_odom_line_lead.publish(line_marker)
 
 if __name__ == "__main__":
     rospy.init_node("odometry_plot",anonymous=False)
 
-    point_list = []
+    point_list_follow = []
+    point_list_lead = []
 
     # Square
     route_list = [(0, 7), (0, -21), (28, -21), (28, 7), (0, 7)]
 
     # Circle
 
-    
-    rospy.Subscriber("/MONICA/localization_gps_imu/odometry", Odometry, cb_odom, queue_size=1)
+    rospy.Subscriber("/MONICA/localization_gps_imu/odometry", Odometry, cb_odom_follow, queue_size=1)
+    rospy.Subscriber("/BRIAN/localization_gps_imu/odometry", Odometry, cb_odom_lead, queue_size=1)
 
-    pub_odom_line = rospy.Publisher("/odom_linelist", Marker, queue_size=1)
     pub_route_line = rospy.Publisher("/route_linelist", Marker, queue_size=1)
 
+    pub_odom_line_follow = rospy.Publisher("/odom_linelist_follow", Marker, queue_size=1)
+    pub_odom_line_lead = rospy.Publisher("/odom_linelist_lead", Marker, queue_size=1)
     
     rospy.Timer(rospy.Duration(1.0), draw)
 
