5bf601141eaf4accd9b23b342aaef3830d11dbd6
Tony Hsiao <tonycar12002@gmail.com>
2019-02-20 03:21:57 +0800

    update

---
+++ catkin_ws/src/analysis/src/path_extract.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import numpy as np
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point
+from nav_msgs.msg import Path
+from message_filters import ApproximateTimeSynchronizer, TimeSynchronizer
+import message_filters
+import math
+import tf
+fo = open("combine_lidar_circle.txt", "w")
+def cb_odom(msg_follow, msg_lead):
+    dis = distance(msg_follow.poses[len(msg_follow.poses)-1].pose.position, msg_lead.poses[len(msg_lead.poses)-1].pose.position)
+    
+    print(dis)
+
+    text = str(dis) + '\n'
+    fo.write(text)
+
+def distance(position_1, position_2):
+    x = position_1.x - position_2.x
+    y = position_1.y - position_2.y
+    return math.sqrt(x*x + y*y)
+
+if __name__ == "__main__":
+    rospy.init_node("odometry_plot",anonymous=False)
+    
+    point_list_follow = []
+    point_list_lead = []
+
+    # Square
+    route_list = [(0, 7), (0, -21), (28, -21), (28, 7), (0, 7)]
+
+    # Circle
+    sub_odom_follow = message_filters.Subscriber("/path_a", Path)
+    sub_odom_lead = message_filters.Subscriber("/path_b", Path)
+
+    ats = ApproximateTimeSynchronizer((sub_odom_follow, sub_odom_lead), queue_size = 1, slop = 0.5)
+    ats.registerCallback(cb_odom)
+
+    rospy.spin()
+
+fo.close()
