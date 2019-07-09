57f4a37f4a8ba063940758caa5f3d73a77cb9d91
huangjuite <huangjuite@gmail.com>
2019-07-10 03:50:20 +0800

    update vrx moos

---
+++ moos/missions/obstacle_manager/alpha_feature.sh
@@ -0,0 +1,7 @@
+
+#uPokeDB alpha.moos TRACKED_FEATURE="x=0, y=-50, label=f, size=5, color=green"
+#uPokeDB alpha.moos TRACKED_FEATURE="x=-50, y=-30, label=a, size=5, color=red"
+uPokeDB alpha.moos TRACKED_FEATURE="x=60, y=-75, label=b, size=5, color=red"
+uPokeDB alpha.moos TRACKED_FEATURE="x=50, y=-35, label=c, size=5, color=green"
+uPokeDB alpha.moos TRACKED_FEATURE="x=100, y=-205, label=d, size=5, color=green"
+uPokeDB alpha.moos TRACKED_FEATURE="x=100, y=-35, label=e, size=5, color=red"
---
+++ moos/missions/vrx_waypt/pop_obstacle.sh
@@ -0,0 +1,8 @@
+uPokeDB vehicle.moos TRACKED_FEATURE="x=72, y=-78, label=a, size=5, color=red"
+uPokeDB vehicle.moos TRACKED_FEATURE="x=69, y=-68, label=b, size=5, color=red"
+uPokeDB vehicle.moos TRACKED_FEATURE="x=41, y=-80, label=c, size=5, color=red"
+uPokeDB vehicle.moos TRACKED_FEATURE="x=45, y=-89, label=d, size=5, color=red"
+uPokeDB vehicle.moos TRACKED_FEATURE="x=57, y=-129, label=f, size=5, color=red"
+uPokeDB vehicle.moos TRACKED_FEATURE="x=65, y=-149, label=g, size=5, color=green"
+uPokeDB vehicle.moos TRACKED_FEATURE="x=61, y=-138, label=h, size=5, color=green"
+uPokeDB vehicle.moos TRACKED_FEATURE="x=94, y=-126, label=i, size=5, color=red"
